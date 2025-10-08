3a0c140b057d242585bedacb4ec2164b4c1ab6db
Joaquin Daniel Cañon Forero <Danielcf97@hotmail.com>
2025-10-08 10:46:19 -0500

    Add files via upload

--- sena_semilleros.sql
+++ sena_semilleros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-10-2025 a las 16:58:30
+-- Tiempo de generación: 08-10-2025 a las 17:44:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -42,7 +42,6 @@ CREATE TABLE `documentos` (
 
 CREATE TABLE `grupos` (
   `id_grupo` int(10) UNSIGNED NOT NULL,
-  `id_proyecto` int(10) UNSIGNED NOT NULL,
   `nombre_grupo` varchar(150) NOT NULL,
   `descripcion` text DEFAULT NULL,
   `estado` enum('ACTIVO','INACTIVO') DEFAULT 'ACTIVO',
@@ -73,6 +72,7 @@ CREATE TABLE `grupo_aprendices` (
 CREATE TABLE `proyectos` (
   `id_proyecto` int(10) UNSIGNED NOT NULL,
   `id_semillero` int(10) UNSIGNED NOT NULL,
+  `id_grupo` int(10) UNSIGNED DEFAULT NULL,
   `nombre_proyecto` varchar(200) NOT NULL,
   `descripcion` text DEFAULT NULL,
   `estado` enum('EN_FORMULACION','EN_EJECUCION','FINALIZADO','ARCHIVADO') DEFAULT 'EN_FORMULACION',
@@ -97,6 +97,13 @@ CREATE TABLE `semilleros` (
   `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `semilleros`
+--
+
+INSERT INTO `semilleros` (`id_semillero`, `nombre_semillero`, `descripcion`, `id_lider`, `estado`, `creado_en`) VALUES
+(1, 'Producción de tecnología  ', NULL, 1, 'ACTIVO', '2025-10-08 15:41:40');
+
 -- --------------------------------------------------------
 
 --
@@ -115,6 +122,14 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `correo`, `password_hash`, `rol`, `estado`, `creado_en`, `actualizado_en`) VALUES
+(1, 'JOSE', 'jose1@HOTMAIL.COM', '123', 'LIDER', 'ACTIVO', '2025-10-08 15:40:41', '2025-10-08 15:40:41'),
+(2, 'JOAQUIN', 'CAÑON', '1234', 'APRENDIZ', 'ACTIVO', '2025-10-08 15:40:41', '2025-10-08 15:40:41');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -129,8 +144,7 @@ ALTER TABLE `documentos`
 -- Indices de la tabla `grupos`
 --
 ALTER TABLE `grupos`
-  ADD PRIMARY KEY (`id_grupo`),
-  ADD UNIQUE KEY `id_proyecto` (`id_proyecto`);
+  ADD PRIMARY KEY (`id_grupo`);
 
 --
 -- Indices de la tabla `grupo_aprendices`
@@ -145,6 +159,7 @@ ALTER TABLE `grupo_aprendices`
 --
 ALTER TABLE `proyectos`
   ADD PRIMARY KEY (`id_proyecto`),
+  ADD UNIQUE KEY `id_grupo` (`id_grupo`),
   ADD KEY `fk_proyecto_semillero` (`id_semillero`);
 
 --
@@ -194,13 +209,13 @@ ALTER TABLE `proyectos`
 -- AUTO_INCREMENT de la tabla `semilleros`
 --
 ALTER TABLE `semilleros`
-  MODIFY `id_semillero` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_semillero` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -213,12 +228,6 @@ ALTER TABLE `documentos`
   ADD CONSTRAINT `fk_documento_proyecto` FOREIGN KEY (`id_proyecto`) REFERENCES `proyectos` (`id_proyecto`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `grupos`
---
-ALTER TABLE `grupos`
-  ADD CONSTRAINT `fk_grupo_proyecto` FOREIGN KEY (`id_proyecto`) REFERENCES `proyectos` (`id_proyecto`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `grupo_aprendices`
 --
 ALTER TABLE `grupo_aprendices`
@@ -229,6 +238,7 @@ ALTER TABLE `grupo_aprendices`
 -- Filtros para la tabla `proyectos`
 --
 ALTER TABLE `proyectos`
+  ADD CONSTRAINT `fk_proyecto_grupo` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id_grupo`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_proyecto_semillero` FOREIGN KEY (`id_semillero`) REFERENCES `semilleros` (`id_semillero`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
